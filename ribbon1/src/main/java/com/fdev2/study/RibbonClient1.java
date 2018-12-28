341fca69dc942da5b3c68689d492740548294492
USER <user@AD01408560.local>
2018-12-28 20:03:48 +0900

    2018.12.28

--- ribbon1/src/main/java/com/fdev2/study/RibbonClient1.java
+++ ribbon1/src/main/java/com/fdev2/study/RibbonClient1.java
@@ -9,6 +9,7 @@ import com.netflix.loadbalancer.IPing;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.PingUrl;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -20,8 +21,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.validation.constraints.Null;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * write description
@@ -58,33 +63,95 @@ class RibbonConfig {
 class RibbonTestController {
     private final RibbonTestService service;
 
-    @GetMapping("/")
-    public String invoke() {
-        return service.invoke();
+    @GetMapping("/ok")
+    public String ok() {
+        return "OK";
+    }
+
+    @GetMapping("/badrequest")
+    public String badRequest() {
+        return service.badRequest();
+    }
+
+    @GetMapping("/timeout")
+    public String timeout() {
+        return service.timeout();
+    }
+
+    @GetMapping("/rejected")
+    public String rejected() {
+        return service.rejected();
+    }
+
+    @GetMapping("/excepted")
+    public String excepted() {
+        return service.excepted();
     }
 }
 
+
 @Service
-@AllArgsConstructor
 class RibbonTestService {
     private final RestTemplate template;
 
+    AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    public RibbonTestService(RestTemplate template) {
+        this.template = template;
+    }
+
+    @HystrixCommand(
+            ignoreExceptions = RuntimeException.class,
+            fallbackMethod = "fallBack",
+            commandProperties = {@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "10")}
+    )
+    public String badRequest() {
+        throw new RuntimeException("badRequest");
+    }
+
+
     @HystrixCommand(
             fallbackMethod = "fallBack",
-            commandProperties = {
-                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "10")
-            }
+            commandProperties = {@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "10")}
     )
-    public String invoke() {
-        for (int i = 0; i < 10000; i++)
-            System.out.println(template.getForObject("http://counter/get", String.class));
+    public String timeout() {
+        try {
+            Thread.sleep(10000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        return "OK";
+        return "timeout";
+    }
+
+
+    @HystrixCommand(
+            fallbackMethod = "fallBack",
+            commandProperties = {@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "10")}
+    )
+    public String rejected() {
+        long value = counter.incrementAndGet();
+
+        if(value%3 == 0)
+            throw new RuntimeException("3's mutiplied");
+
+
+        return "rejected";
+    }
+
+
+    @HystrixCommand(
+            fallbackMethod = "fallBack",
+            commandProperties = {@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "10")}
+    )
+    public String excepted() throws RuntimeException {
+        throw new RuntimeException("excepted");
     }
 
 
     public String fallBack() {
-        return "server 1: fall backed";
+        return "fallback";
     }
 
 }
